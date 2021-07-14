a4495949c1af80d01f954778ed52dc4adeb935ca
gurjyan <gurjyan@jlab.org>
2021-07-14 07:27:30 -0400

    - debugging

--- service/src/jana_service.hpp
+++ service/src/jana_service.hpp
@@ -14,7 +14,7 @@ namespace ersap {
     namespace jana {
 
 
-class JanaEngine;
+//class JanaEngine;
 
 class JanaService : public ersap::Engine
 {
@@ -52,8 +52,8 @@ public:
 
     std::string version() const override;
 
-private:
-    std::shared_ptr<JanaEngine> engine_{};
+//private:
+//    std::shared_ptr<JanaEngine> engine_{};
 };
 
     } // end namespace jana
