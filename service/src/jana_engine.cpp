506e97334402030c044d6fb5097f6258f738a7db
gurjyan <gurjyan@jlab.org>
2021-07-16 14:38:12 -0400

    - debugging

---
+++ service/src/jana_engine.cpp
@@ -0,0 +1,12 @@
+#include "jana_engine.hpp"
+#include <iostream>
+
+namespace ersap {
+    namespace jana {
+
+        void JanaEngine::process() {
+            std::cout << "Running Jana Processor" << std::endl;
+        }
+
+    } // end namespace jana
+} // end namespace ersap
---
+++ service/src/jana_engine.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Vardan Gurjyan on 7/16/21.
+//
+
+#ifndef ERSAP_JANA_JANA_ENGINE_HPP
+#define ERSAP_JANA_JANA_ENGINE_HPP
+namespace ersap {
+    namespace jana {
+
+        class JanaEngine {
+        public:
+            /**
+             * does something
+             */
+            void process();
+        };
+
+    } // end namespace jana
+} // end namespace ersap
+
+#endif //ERSAP_JANA_JANA_ENGINE_HPP
