408156b6d05f219d4b59d8a857b5500d4730eb11
Nathan Brei <nbrei@jlab.org>
2021-09-10 00:54:50 -0400

    sampa_service_tests now finds sampa_service

--- services/src/test/cpp/sampa_service_tests.cc
+++ services/src/test/cpp/sampa_service_tests.cc
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "sampa_data_type.hpp"
+#include "sampa_service.hpp"
 
 using namespace ersap::jana;
 
@@ -13,11 +14,13 @@ TEST(SampaServiceTests, BasicAssertions) {
     original.payload = {{1,2,3}, {7,8,9,10}};
     original.timeslice_id = 22;
 
-    auto sut = SampaSerializer();
-    auto serialized = sut.write(original);
-    auto deserialized = ersap::any_cast<SampaDASMessage>(sut.read(serialized));
+    auto sut = SampaService();
+    ersap::EngineData ed;
+    // TODO: Figure out how to insert and retrieve data + metadata from EngineData object
+    auto result = sut.execute(ed);
 
-    EXPECT_EQ(original.payload, deserialized.payload);
+    // TODO: Decide on what to test. Maybe ed should contain the sum of all values in original.payload
+    EXPECT_EQ(1,1);
 
 }
 
